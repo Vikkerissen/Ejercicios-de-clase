cf316047022a3e59dbf02382f0d551879e12ce53
Vikkerissen <Vikkerissen32@gmail.com>
2017-04-03 15:39:04 -0300

    Add files via upload

---
+++ Ejercicio02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int num1, num2, num3, sum, mult;
+
+printf("\nIngrese el primer numero: ");
+scanf("%d", &num1);
+
+printf("\nIngrese el segundo numero: ");
+scanf("%d", &num2);
+
+printf("\nIngrese el tercer numero: ");
+scanf("%d", &num3);
+
+sum = num1+num2+num3;
+mult = num1*num2*num3;
+
+printf("\nLa suma de los numeros es: %d", sum);
+printf("\nLa multiplicacion de los numeros es: %d", mult);
+
+getch();
+
+}
