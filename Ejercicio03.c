c08be23a0e8dd2e7ed66f51b1658dfe92b388dc8
Vikkerissen <Vikkerissen32@gmail.com>
2017-04-03 16:03:17 -0300

    Add files via upload

---
+++ Ejercicio03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int a, b, suma, resta, multi;
+
+printf("\n1) Ingrese el valor de A: ");
+scanf("%d", &a);
+
+printf("\n2) Ingrese el valor de B: ");
+scanf("%d", &b);
+
+suma = a+b;
+resta = a-b;
+multi = a*b;
+
+printf("\nEl resultado de la suma es: %d, el de la resta: %d y el de la multiplicacion: %d", suma, resta, multi);
+
+getch();
+
+}
